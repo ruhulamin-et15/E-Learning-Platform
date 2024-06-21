5034b6fcc032df99ab6c509ac9b102a5852bae5f
ruhulamin-et15 <ruhulamin.et15@gmail.com>
2024-06-22 01:11:22 +0600

    update auth

--- auth.js
+++ auth.js
@@ -1,49 +1,49 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import CredentialsProvider from "next-auth/providers/credentials";
-// import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 import { User } from "./model/user-model";
 import bcrypt from "bcryptjs";
 
-// async function refreshAccessToken(token) {
-//   try {
-//     const url =
-//       "https://oauth2.googleapis.com/token?" +
-//       new URLSearchParams({
-//         client_id: process.env.GOOGLE_CLIENT_ID,
-//         client_secret: process.env.GOOGLE_CLIENT_SECRET_ID,
-//         grant_type: "refresh_token",
-//         refresh_token: token.refreshToken,
-//       });
+async function refreshAccessToken(token) {
+  try {
+    const url =
+      "https://oauth2.googleapis.com/token?" +
+      new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET_ID,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      });
 
-//     const response = await fetch(url, {
-//       headers: {
-//         "Content-Type": "application/x-www-form-urlencoded",
-//       },
-//       method: "POST",
-//     });
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    });
 
-//     const refreshedTokens = await response.json();
+    const refreshedTokens = await response.json();
 
-//     if (!response.ok) {
-//       throw refreshedTokens;
-//     }
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
 
-//     return {
-//       ...token,
-//       accessToken: refreshedTokens?.access_token,
-//       accessTokenExpires: Date.now() + refreshedTokens?.expires_in * 1000,
-//       refreshToken: refreshedTokens?.refresh_token,
-//     };
-//   } catch (error) {
-//     console.log(error);
+    return {
+      ...token,
+      accessToken: refreshedTokens?.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens?.expires_in * 1000,
+      refreshToken: refreshedTokens?.refresh_token,
+    };
+  } catch (error) {
+    console.log(error);
 
-//     return {
-//       ...token,
-//       error: "RefreshAccessTokenError",
-//     };
-//   }
-// }
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export const {
   auth,
@@ -85,54 +85,54 @@ export const {
         }
       },
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET_ID,
-    //   authorization: {
-    //     params: {
-    //       prompt: "consent",
-    //       access_type: "offline",
-    //       response_type: "code",
-    //     },
-    //   },
-    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET_ID,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
 
-  // callbacks: {
-  //   async jwt({ token, user, account }) {
-  //     console.log(`JWT token: ${JSON.stringify(token)}`);
-  //     console.log(`JWT Account: ${JSON.stringify(account)}`);
+  callbacks: {
+    async jwt({ token, user, account }) {
+      console.log(`JWT token: ${JSON.stringify(token)}`);
+      console.log(`JWT Account: ${JSON.stringify(account)}`);
 
-  //     if (account && user) {
-  //       return {
-  //         accessToken: account?.access_token,
-  //         accessTokenExpires: Date.now() + account?.expires_in * 1000,
-  //         refreshToken: account?.refresh_token,
-  //         user,
-  //       };
-  //     }
+      if (account && user) {
+        return {
+          accessToken: account?.access_token,
+          accessTokenExpires: Date.now() + account?.expires_in * 1000,
+          refreshToken: account?.refresh_token,
+          user,
+        };
+      }
 
-  //     console.log(
-  //       `Token Will Expire at ${new Date(token.accessTokenExpires)})`
-  //     );
+      console.log(
+        `Token Will Expire at ${new Date(token.accessTokenExpires)})`
+      );
 
-  //     if (Date.now() < token?.accessTokenExpires) {
-  //       console.log(`At ${new Date(Date.now())}, Using old access token`);
-  //       return token;
-  //     }
+      if (Date.now() < token?.accessTokenExpires) {
+        console.log(`At ${new Date(Date.now())}, Using old access token`);
+        return token;
+      }
 
-  //     console.log(`Token Expired at ${new Date(Date.now())}`);
-  //     return refreshAccessToken(token);
-  //   },
+      console.log(`Token Expired at ${new Date(Date.now())}`);
+      return refreshAccessToken(token);
+    },
 
-  //   async session({ session, token }) {
-  //     session.user = token?.user;
-  //     session.accessToken = token?.access_token;
-  //     session.error = token?.error;
+    async session({ session, token }) {
+      session.user = token?.user;
+      session.accessToken = token?.access_token;
+      session.error = token?.error;
 
-  //     console.log(`Returning Session ${JSON.stringify(session)}`);
-  //     return session;
-  //   },
-  // },
+      console.log(`Returning Session ${JSON.stringify(session)}`);
+      return session;
+    },
+  },
 });
 
